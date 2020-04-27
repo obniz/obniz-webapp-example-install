8579deee42edd7bfdaed0e2966425ef00d184ce0
takasago <vipqiv00@gmail.com>
2020-04-27 16:03:38 +0900

    FIX: BullのRedis指定

--- dist/src/queues.js
+++ dist/src/queues.js
@@ -5,6 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const bull_1 = __importDefault(require("bull"));
 exports.default = {
-    installQueue: new bull_1.default("install"),
-    taskQueue: new bull_1.default("task"),
+    installQueue: new bull_1.default("install", process.env.REDIS_URL),
+    taskQueue: new bull_1.default("task", process.env.REDIS_URL),
 };
--- src/queues.ts
+++ src/queues.ts
@@ -1,6 +1,7 @@
 import Queue from "bull";
+import Redis from "./redis";
 
 export default {
-  installQueue: new Queue("install"),
-  taskQueue: new Queue("task"),
+  installQueue: new Queue("install", process.env.REDIS_URL!),
+  taskQueue: new Queue("task", process.env.REDIS_URL!),
 };
